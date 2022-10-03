d4ab1affe7c04bfe205fe3494c5ca809db73546a
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2022-10-02 23:23:41 -0400

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        for (int i = 5; i != 0; i--) {
+        if (i == 3) {
+            System.out.println("Mariz!");
+            break;
+            }
+            System.out.println("Bianca");
+                }
+        
+    
+        int nicknameCount = 5; {
+        while (nicknameCount != 0) {
+            nicknameCount--;
+            }
+            for (int i = 5; i != 0; i--) {
+                if (i == 3) {
+                    System.out.println("Biang!");
+                    continue;
+                }
+                System.out.println("Yayang");
+            }
+        }
+    
+
+    {
+        int surnameCount = 4;
+
+        do {
+            System.out.println("Villanueva");
+            surnameCount--;
+        } while (surnameCount != 0);
+        }
+    }
+}
+
+//sir it works fine this way (doesn't loop infinitely)
