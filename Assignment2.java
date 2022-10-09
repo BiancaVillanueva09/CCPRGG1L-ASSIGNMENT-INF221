c748bf3fcd185cd07acf37338ceff446ea052851
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2022-10-10 02:11:51 +0800

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,38 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    
+
         for (int i = 5; i != 0; i--) {
-        if (i == 3) {
+            if (i == 2) {
             System.out.println("Mariz!");
             break;
             }
             System.out.println("Bianca");
-                }
+            } 
         
     
-        int nicknameCount = 5; {
-        while (nicknameCount != 0) {
-            nicknameCount--;
-            }
+        int surnameCount = 5; {
+        while (surnameCount != 0) {surnameCount--;}
             for (int i = 5; i != 0; i--) {
-                if (i == 3) {
-                    System.out.println("Biang!");
-                    continue;
-                }
-                System.out.println("Yayang");
+            if (i == 3) {
+            System.out.println("Mariz");
+            continue;
             }
-        }
+            System.out.println("Villanueva");
+            }
+            }
     
 
-       {
-        int surnameCount = 5;
-
+        int nicknameCount = 5; {
         do {
-            System.out.println("Villanueva");
-            surnameCount--;
-        } while (surnameCount != 0);
-        }
+            System.out.println("Yayang");
+            nicknameCount--;
+            } 
+            while (nicknameCount != 0);
+            }
     }
 }
-
-//sir it works fine this way (doesn't loop infinitely)
